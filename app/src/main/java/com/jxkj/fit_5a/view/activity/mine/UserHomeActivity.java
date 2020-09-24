8bede63ca029b0032507c44f246ad4287acba215
qianzhanjie <qzj842179561@gmail.com>
2020-09-24 10:03:20 +0800

    ui

--- app/src/main/java/com/jxkj/fit_5a/view/activity/mine/UserHomeActivity.java
+++ app/src/main/java/com/jxkj/fit_5a/view/activity/mine/UserHomeActivity.java
@@ -115,7 +115,6 @@ public class UserHomeActivity extends BaseActivity {
         HomeThreeSqAdapter mHomeThreeSqAdapter = new HomeThreeSqAdapter(list);
         mRvDtListSp.setHasFixedSize(true);
         mRvDtListSp.setAdapter(mHomeThreeSqAdapter);
-
         mHomeThreeSqAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
