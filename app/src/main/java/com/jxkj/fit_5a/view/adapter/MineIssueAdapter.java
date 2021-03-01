25504116164fb73bafabc87f619b5537622e183b
qianzhanjie <qzj842179561@gmail.com>
2021-03-01 10:16:05 +0800

    ui

--- app/src/main/java/com/jxkj/fit_5a/view/adapter/MineIssueAdapter.java
+++ app/src/main/java/com/jxkj/fit_5a/view/adapter/MineIssueAdapter.java
@@ -1,6 +1,9 @@
 package com.jxkj.fit_5a.view.adapter;
 
+import android.os.Build;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,6 +13,7 @@ import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.jxkj.fit_5a.R;
 import com.jxkj.fit_5a.base.HelpListData;
+import com.jxkj.fit_5a.conpoment.utils.StringUtil;
 
 import java.util.List;
 
@@ -26,19 +30,32 @@ public class MineIssueAdapter extends BaseQuickAdapter<HelpListData.ListBean, Ba
     protected void convert(@NonNull BaseViewHolder helper, HelpListData.ListBean item) {
 
         helper.setText(R.id.tv_title,item.getTitle())
-                .setText(R.id.tv_content,item.getDetail())
                 .setText(R.id.tv_num,(getLoadMoreViewPosition()+1)+"");
-        TextView tvC = helper.getView(R.id.tv_content);
+        WebView mVebView = helper.getView(R.id.tv_content);
+        initWebView(mVebView,item.getDetail());
         helper.getView(R.id.tv_title).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(tvC.getVisibility()==View.VISIBLE){
-                    tvC.setVisibility(View.GONE);
+                if(mVebView.getVisibility()==View.VISIBLE){
+                    mVebView.setVisibility(View.GONE);
                 }else{
-                    tvC.setVisibility(View.VISIBLE);
+                    mVebView.setVisibility(View.VISIBLE);
                 }
             }
         });
+    }
+
+    private void initWebView(WebView mVebView,String detail) {
+        WebSettings webSettings = mVebView.getSettings();//获取webview设置属性
+        webSettings.setDefaultTextEncodingName("UTF-8");//设置默认为utf-8
+        webSettings.setBlockNetworkImage(false); // 解决图片不显示
+        //支持javascript
+//        webSettings.setJavaScriptEnabled(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+        mVebView.loadData(
+                StringUtil.getNewContent1(detail), "text/html; charset=UTF-8", null);//这种写法可以正确解码
     }
 
 }
