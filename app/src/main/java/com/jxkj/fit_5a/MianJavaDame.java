907bb4dec21160539c46013fa3ac5dd4beb88583
qianzhanjie <qzj842179561@gmail.com>
2021-08-05 15:28:35 +0800

    ui

--- app/src/main/java/com/jxkj/fit_5a/MianJavaDame.java
+++ app/src/main/java/com/jxkj/fit_5a/MianJavaDame.java
@@ -1,12 +1,17 @@
 package com.jxkj.fit_5a;
 
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
 public class MianJavaDame {
+    Thread thread=new Thread();
+    static String newString = "_";//新字符串
+    static String oldString = " ";//要被替换的字符串
+    static String dir = "C:\\Users\\Administrator\\Downloads\\图标";//文件所在路径，所有文件的根目录
     public static void main(String[] args) {
 //        int[] test = {10,-200,3,8,9,2,5};
 //        Arrays.sort(test); //首先对数组排序
@@ -17,11 +22,43 @@ public class MianJavaDame {
 ////        System.out.println("-->>>:"+(float)a*b); //这里的结果是 3
 //        int number = 10;
 //        System.out.println("-->>>:"+ Math.floor(1.4));
-        System.out.println("-->>>:"+(int) (129/130));
 
 //        System.out.println("-->>>:"+"getDate"+ (float) 300 / 500.0f);
 //        System.out.println("-->>>:"+("0000fff1-0000-1000-8000-00805f9b34fb".indexOf("fff1")));
+        recursiveTraversalFolder(dir);//递归遍历此路径下所有文件夹
+    }
 
+    public static void recursiveTraversalFolder(String path) {
+        File folder = new File(path);
+        if (folder.exists()) {
+            File[] fileArr = folder.listFiles();
+            if (null == fileArr || fileArr.length == 0) {
+                System.out.println("文件夹是空的!");
+                return;
+            } else {
+                File newDir = null;//文件所在文件夹路径+新文件名
+                String newName = "";//新文件名
+                String fileName = null;//旧文件名
+                File parentPath = new File("");//文件所在父级路径
+                for (File file : fileArr) {
+                    if (file.isDirectory()) {//是文件夹，继续递归，如果需要重命名文件夹，这里可以做处理
+                        System.out.println("文件夹:" + file.getAbsolutePath() + "，继续递归！");
+                        recursiveTraversalFolder(file.getAbsolutePath());
+                    } else {//是文件，判断是否需要重命名
+                        fileName = file.getName();
+                        parentPath = file.getParentFile();
+                        if (fileName.contains(oldString)) {//文件名包含需要被替换的字符串
+                            newName = fileName.replaceAll(oldString, newString);//新名字
+                            newDir = new File(parentPath + "/" + newName.toLowerCase());//文件所在文件夹路径+新文件名
+                            file.renameTo(newDir);//重命名
+                            System.out.println("修改后：" + newDir);
+                        }
+                    }
+                }
+            }
+        } else {
+            System.out.println("文件不存在!");
+        }
     }
 
     public static String getValue(double obj) {
